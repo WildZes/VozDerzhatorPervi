8d055e181e33bf70a1683e3aeb735785bad459af
wildzes <wildzes@gmail.com>
2022-11-08 00:46:47 +0300

    Date picker, main window and task window visually almost done

--- screenmanager_helping.py
+++ screenmanager_helping.py
@@ -19,9 +19,24 @@ class MainScreen(Screen):
 class VDScreen(Screen):
     btn = ObjectProperty(None)
 
+    def select_date(self):
+        picker = MDDatePicker(mode="range")
+        picker.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        picker.open()
+
+
+    def on_save(self, instance, value, date_range):
+        # print(instance, value, date_range)
+        # self.root.ids.date_label.text = str(value)
+        self.ids.c_date.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+
+
+    def on_cancel(self, instance, value):
+        print(instance, value)
+        # self.root.ids.c_date.text = "You Clicked Cancel"
+
 
 class ScreenManagerApp(MDApp):
-
     def build(self):
         root = ScreenManager()
         root.add_widget(MainScreen(name='main'))
